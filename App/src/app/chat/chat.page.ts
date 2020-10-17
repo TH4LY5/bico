89cf03156c001670405e4e9707c4670574792843
TH4LY5 <30905866+TH4LY5@users.noreply.github.com>
2020-10-17 14:11:18 -0300

    chat
    Precisa de melhorar o sistema de chat

--- App/src/app/chat/chat.page.ts
+++ App/src/app/chat/chat.page.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
+  messages = [
+    {
+      side : 'right',
+      msg : 'Olá'
+    },
+    {
+      side : 'left',
+      msg : 'Oie'
+    },
+    {
+      side : 'right',
+      msg : 'Queria um orçamento, teria como fazer ?'
+    },
+    {
+      side : 'left',
+      msg : 'Claro! Vou enviar uma proposta!'
+    },
+    {
+      side : 'right',
+      msg : 'Ok, vou aguarda, obrigado'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
